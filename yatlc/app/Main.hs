847aaf4c48aa5de9b93d5579c88b6149a41e2f6a
Raphael Meyer <raphael.akira.meyer@gmail.com>
2026-06-05 08:17:30 +0200

    add PoC for compilation pipeline

--- yatlc/app/Main.hs
+++ yatlc/app/Main.hs
@@ -1,11 +1,16 @@
 module Main where
 
+import qualified Data.ByteString as BS
 import qualified Data.Text as Text
+import qualified Generator.Generator as Generator
 import qualified Parser.Parser as Parser
-import qualified Parser.Parser as Parser.Parser
+import qualified System.IO as SysIO
 
 main :: IO ()
 main = do
   putStrLn "yatlc"
-  let _ = Parser.parse Text.empty
-  pure ()
+  let ast = Parser.parse Text.empty
+  let wasm = Generator.emit ast
+  SysIO.withBinaryFile "/tmp/foo/foo.wasm" SysIO.WriteMode (`BS.hPut` wasm)
+  let wit = Generator.wit ast
+  writeFile "/tmp/foo/foo.wit" (Text.unpack wit)
