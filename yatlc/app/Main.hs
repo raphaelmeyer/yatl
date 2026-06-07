9b9eebc3a467d0df8d653260dcf0a9a84ec470dd
Raphael Meyer <raphael.akira.meyer@gmail.com>
2026-06-07 19:57:03 +0200

    split taskfile

--- yatlc/app/Main.hs
+++ yatlc/app/Main.hs
@@ -4,13 +4,23 @@ import qualified Data.ByteString as BS
 import qualified Data.Text as Text
 import qualified Generator.Generator as Generator
 import qualified Parser.Parser as Parser
+import qualified System.Environment as Environment
+import qualified System.Exit as Exit
+import System.FilePath ((</>))
 import qualified System.IO as SysIO
 
 main :: IO ()
 main = do
-  putStrLn "yatlc"
+  args <- Environment.getArgs
+  case args of
+    [dir] -> compileFile dir
+    _ -> Exit.exitFailure
+
+compileFile :: FilePath -> IO ()
+compileFile workDir = do
+  putStrLn $ "yatlc: working directory '" ++ workDir ++ "'"
   let ast = Parser.parse Text.empty
   let wasm = Generator.emit ast
-  SysIO.withBinaryFile "/tmp/foo/foo.wasm" SysIO.WriteMode (`BS.hPut` wasm)
+  SysIO.withBinaryFile (workDir </> "foo.wasm") SysIO.WriteMode (`BS.hPut` wasm)
   let wit = Generator.wit ast
-  writeFile "/tmp/foo/foo.wit" (Text.unpack wit)
+  writeFile (workDir </> "foo.wit") (Text.unpack wit)
