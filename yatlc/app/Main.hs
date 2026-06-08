b8ccfc6ef4c491eb9e35b19dc00b5c766e2e12df
Raphael Meyer <raphael.akira.meyer@gmail.com>
2026-06-08 08:39:06 +0200

    use optparse applicative

--- yatlc/app/Main.hs
+++ yatlc/app/Main.hs
@@ -1,20 +1,47 @@
 module Main where
 
+import Control.Applicative ((<**>))
 import qualified Data.ByteString as BS
 import qualified Data.Text as Text
 import qualified Generator.Generator as Generator
+import qualified Options.Applicative as Options
 import qualified Parser.Parser as Parser
-import qualified System.Environment as Environment
-import qualified System.Exit as Exit
 import System.FilePath ((</>))
 import qualified System.IO as SysIO
 
+data Options = Options
+  { optWorkingDirectory :: FilePath,
+    optFile :: FilePath
+  }
+  deriving (Show)
+
+options :: Options.Parser Options
+options =
+  Options
+    <$> ( Options.option Options.str $
+            Options.long "build-directory"
+              <> Options.short 'b'
+              <> Options.help "Directory for build artifacts"
+              <> Options.metavar "DIR"
+        )
+    <*> ( Options.argument Options.str $
+            Options.help "File to compile"
+              <> Options.metavar "FILE"
+        )
+
 main :: IO ()
 main = do
-  args <- Environment.getArgs
-  case args of
-    [dir] -> compileFile dir
-    _ -> Exit.exitFailure
+  opts <-
+    Options.execParser
+      ( Options.info
+          (options <**> Options.helper)
+          ( Options.fullDesc
+              <> Options.progDesc "yatl compiler"
+              <> Options.header "yatlc"
+          )
+      )
+
+  compileFile (optWorkingDirectory opts)
 
 compileFile :: FilePath -> IO ()
 compileFile workDir = do
